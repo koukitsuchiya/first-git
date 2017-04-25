37d56d54cdcc56290997586160a10824850c9ebc
jiro.kirameki <t.k.m.thirty530@gmail.com>
2017-04-25 08:10:17 +0000

    kadai-tasklist

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,5 +1,2 @@
 class Tasklist < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 255 }
-  validates :title, presence: true, length: { maximum: 255 }
-  validates :title, presence: true, length: { maximum: 10 }
 end
--- db/migrate/20170424070933_create_tasklists.rb
+++
@@ -1,5 +0,0 @@
-class CreateTasklists < ActiveRecord::Migration[5.0]
-  def change
-   add_column :tasklists, :title, :string
-  end
-end
